5cb65e226bae7c8d062b35d60162f57907aa4654
vuongabc92 <vuongabc92@gmail.com>
2015-07-21 22:51:48 +0700

    Setting > my store > store header

--- packages/King/Frontend/resources/views/store/index.blade.php
+++ packages/King/Frontend/resources/views/store/index.blade.php
@@ -8,7 +8,19 @@ My store
 <div class="_mw970 _ma">
     <div class="_fwfl store-container">
         <div class="_fwfl store-header">
-
+            <div class="_fwfl store-cover-block">
+                <div class="_fwfl cover-backgroud" style="background-image:url('{{ get_cover('big') }}');"></div>
+            </div>
+            <div class="_fwfl store-nav-bar">
+                <ul class="_fwfl _ls store-nav-list">
+                    <li><a href="#">Products <span class="_fs12">(12)</span></a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Rating <span class="_fs12">(2)</span></a></li>
+                    <li><a href="#">Follow <span class="_fs12">(22)</span></a></li>
+                    <li><a href="#"><i class="_fs14 fa fa-plus"></i></a></li>
+                    <li><a href="#"><i class="_fs14 fa fa-search"></i></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
